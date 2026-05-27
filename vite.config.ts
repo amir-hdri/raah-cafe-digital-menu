ebc3a862487c2d4f122b10d06c358052f71c6be9
Amir hossein Heidari <168375652+amir-hdri@users.noreply.github.com>
2026-05-28 02:18:34 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/raah-cafe---digital-menu/', // برای تنظیم مسیر درست در گیت‌هاب پیجز اضافه شد
+    base: '/raah-cafe-digital-menu/', // برای تنظیم مسیر درست در گیت‌هاب پیجز اضافه شد
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
